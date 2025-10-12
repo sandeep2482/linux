ca8053d134007e70f0de0bd2cd67897745918ec2
sandeep2482 <sand.2482@gmail.com>
2025-10-12 17:03:59 +0530

    Added date variable

--- scripts/users.sh
+++ scripts/users.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+TODAY=$(date +%F)
 read -p "Enter a name:" name
 read -p "Enter a office: SSNNN " office
 dept=${office:0:2}
 room=${office:2}
+echo "User Created on $TODAY"
 echo "Name $name"
 echo "Department $dept"
 echo "Room $room"
